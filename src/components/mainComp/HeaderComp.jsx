8e5be5ed13b52d74a5d77140b91f4188a2ae1b92
biooids <biooidsbiooid@gmail.com>
2024-06-13 21:09:34 +0200

    "improve the header components and side bar component"

--- src/components/mainComp/HeaderComp.jsx
+++ src/components/mainComp/HeaderComp.jsx
@@ -1,17 +1,19 @@
 import { Button, Navbar, TextInput } from "flowbite-react";
 import React from "react";
 
-import { ImSearch } from "react-icons/im";
-import { Dropdown } from "flowbite-react";
 import { IoSettings } from "react-icons/io5";
 import { MdOutlineEvent, MdMenuBook } from "react-icons/md";
 import { FaStoreAlt } from "react-icons/fa";
+import { FaHome } from "react-icons/fa";
+
+import { MdNotificationsActive, MdMessage } from "react-icons/md";
+
 import logo from "../../assets/ffwpu.png";
 import { Link } from "react-router-dom";
 
 function HeaderComp() {
   return (
-    <Navbar className="bg-transparent dark:bg-transparent border-b-2  relative flex-wrap">
+    <Navbar className=" dark:bg-transparent border-b-2 border-gray-400 ">
       <Link to="/">
         <Navbar.Brand as={"div"}>
           <img src={logo} className="mr-1 h-6 sm:h-9" alt="ffwpu Logo" />
@@ -20,51 +22,21 @@ function HeaderComp() {
           </span>
         </Navbar.Brand>
       </Link>
-      <form className=" gap-1 flex">
-        <TextInput type="text" placeholder="Search" />
-        <Button
-          type="submit"
-          className="flex justify-center items-center   focus:ring-0"
-        >
-          <ImSearch />
-        </Button>
-      </form>
 
-      <div className="flex">
-        <Link to="events">
-          <Button
-            color="gray "
-            className="hover:bg-amber-900 rounded-l rounded-r-none focus:ring-0"
-          >
-            <MdOutlineEvent className="mr-3 h-4 w-4" />
-            Events
-          </Button>
-        </Link>
-        <Link to="lectures">
-          <Button
-            color="gray "
-            className="hover:bg-amber-900 rounded-none focus:ring-0"
-          >
-            <MdMenuBook className="mr-3 h-4 w-4" />
-            Ectures
-          </Button>
-        </Link>
-        <Link as="div">
-          <Button
-            color="gray "
-            className="hover:bg-amber-900 rounded-l-none rounded-r focus:ring-0"
-          >
-            <FaStoreAlt className="mr-3 h-4 w-4" />
-            Store
-          </Button>
-        </Link>
-      </div>
+      <Link className="hover:bg-amber-900 border-2 p-1 border-amber-500 rounded-md  flex justify-center items-center">
+        <MdOutlineEvent />
+      </Link>
 
-      <div className="flex gap-3 justify-center items-center  ">
-        <Dropdown label={<IoSettings />} className="" color="none">
-          <Dropdown.Item>Light </Dropdown.Item>
-          <Dropdown.Item>Dark</Dropdown.Item>
-        </Dropdown>
+      <Link className="hover:bg-amber-900  border-2 p-1 border-amber-500 rounded-md flex justify-center items-center">
+        <MdMessage />
+      </Link>
+
+      <Link className="hover:bg-amber-900 h-full border-2 p-1 border-amber-500 rounded-md flex justify-center items-center">
+        <MdNotificationsActive />
+      </Link>
+
+      <div className="  text-2xl">
+        <IoSettings />
       </div>
     </Navbar>
   );
--- src/components/mainComp/SidebarComp.jsx
+++ src/components/mainComp/SidebarComp.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { Avatar, Sidebar } from "flowbite-react";
+
 import { FaUserCircle, FaHome, FaStoreAlt } from "react-icons/fa";
-import { MdOutlineEvent, MdOutlinePermMedia, MdMenuBook } from "react-icons/md";
+import {
+  MdMessage,
+  MdOutlinePublic,
+  MdOutlineEvent,
+  MdOutlinePermMedia,
+  MdNotificationsActive,
+  MdHelp,
+  MdMenuBook,
+} from "react-icons/md";
+
 import profilePic from "../../assets/father.jpg";
 import { Link } from "react-router-dom";
 
@@ -9,46 +19,67 @@ function SidebarComp() {
   return (
     <>
       {/* Sidebar for larger screens */}
-      <Sidebar className="sticky top-0 hidden md:block w-[300px]">
-        <div className="border-b-2 border-amber-500 pt-5">
+      <Sidebar className=" sticky top-0  hidden md:block w-[300px]  z-10  ">
+        <div className=" border-b-2 border-amber-500 pt-5">
           <Avatar img={profilePic} size="lg" rounded bordered />
           <div className="text-center">
             <h3>John Doe: Leader</h3>
           </div>
         </div>
-        <Sidebar.Items className="mt-3 h-screen">
+        <Sidebar.Items className="mt-3  h-screen  ">
           <Sidebar.ItemGroup>
-            <Sidebar.Item icon={FaUserCircle} as="div">
-              Update Profile
+            <Sidebar.Item icon={FaUserCircle}>Update Profile</Sidebar.Item>
+
+            <Sidebar.Item icon={FaHome}>Home</Sidebar.Item>
+            <Sidebar.Item icon={MdMessage} label="4" labelColor="dark">
+              Chat
             </Sidebar.Item>
-            <Sidebar.Item icon={FaHome} as="div">
-              Home
-            </Sidebar.Item>
+
             <Link to="events">
-              <Sidebar.Item icon={MdOutlineEvent} as="div">
+              <Sidebar.Item
+                icon={MdOutlineEvent}
+                label="2"
+                labelColor="dark"
+                as={"div"}
+              >
                 Events
               </Sidebar.Item>
             </Link>
             <Link to="talents">
-              <Sidebar.Item icon={MdOutlineEvent} as="div">
+              <Sidebar.Item
+                icon={MdOutlineEvent}
+                label="2"
+                labelColor="dark"
+                as={"div"}
+              >
                 Talents
               </Sidebar.Item>
             </Link>
             <Link to="lectures">
-              <Sidebar.Item icon={MdMenuBook} as="div">
+              <Sidebar.Item icon={MdMenuBook} as={"div"}>
                 Lectures
               </Sidebar.Item>
             </Link>
             <Link to="market">
-              <Sidebar.Item icon={FaStoreAlt} as="div">
+              <Sidebar.Item icon={FaStoreAlt} as={"div"}>
                 Market
               </Sidebar.Item>
             </Link>
             <Link to="media">
-              <Sidebar.Item icon={MdOutlinePermMedia} as="div">
+              <Sidebar.Item icon={MdOutlinePermMedia} as={"div"}>
                 Media
               </Sidebar.Item>
             </Link>
+          </Sidebar.ItemGroup>
+          <Sidebar.ItemGroup>
+            <Sidebar.Item
+              icon={MdNotificationsActive}
+              label="9+"
+              labelColor="dark"
+            >
+              Notifications
+            </Sidebar.Item>
+            <Sidebar.Item icon={MdHelp}>Help</Sidebar.Item>
           </Sidebar.ItemGroup>
         </Sidebar.Items>
       </Sidebar>
@@ -56,8 +87,8 @@ function SidebarComp() {
       {/* Bottom navigation for smaller screens */}
       <div className="fixed bottom-0 left-0 right-0 border-t-2 border-amber-500 md:hidden flex justify-around items-center py-2 z-10">
         <Link to="events">
-          <MdOutlineEvent className="text-xl" />
-          <span className="text-xs">Events</span>
+          <FaHome className="text-xl" />
+          <span className="text-xs">Home</span>
         </Link>
         <Link to="talents">
           <MdOutlineEvent className="text-xl" />
