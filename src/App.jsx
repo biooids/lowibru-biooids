6cc864c8ec86b7995f5b550c260af98d7ef0bf99
biooids <biooidsbiooid@gmail.com>
2024-07-05 05:26:30 +0200

    add complete market place sections

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,11 @@ import Discover from "./components/pages/lectures/components/Discover";
 import MyLectures from "./components/pages/lectures/components/MyLectures";
 import UploadLectures from "./components/pages/lectures/components/UploadLectures";
 import Lesson from "./components/pages/lectures/components/Lesson";
+import Market from "./components/pages/market/Market";
+import AllItems from "./components/pages/market/components/AllItems";
+import MyCart from "./components/pages/market/components/MyCart";
+import Status from "./components/pages/market/components/Status";
+import UploadItems from "./components/pages/market/components/UploadItems";
 
 function App() {
   return (
@@ -26,6 +31,7 @@ function App() {
         <Route path="/" element={<MainLayout />}>
           <Route index element={<HomePage />}></Route>
 
+          {/* Events */}
           <Route path="/events" element={<Events />}>
             <Route index element={<Activities />} />
             <Route path="live" element={<LiveEvents />} />
@@ -42,11 +48,22 @@ function App() {
             </Route>
           </Route>
 
+          {/* Lectures */}
           <Route path="/lectures" element={<Lectures />}>
             <Route index element={<Discover />} />
             <Route path="mylectures" element={<MyLectures />} />
             <Route path="mylectures/:id" element={<Lesson />} />
             <Route path="upload" element={<UploadLectures />} />
+          </Route>
+
+          {/* Market */}
+          <Route path="/market" element={<Market />}>
+            <Route index element={<AllItems />} />
+            <Route path="mycart" element={<MyCart />} />
+            <Route path="status" element={<Status />} />
+            <Route path="uploaditems" element={<UploadItems />} />
+            {/*
+             */}
           </Route>
         </Route>
       </Routes>
