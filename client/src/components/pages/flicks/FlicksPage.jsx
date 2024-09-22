49e164c01df65e97652123d1817f096d6eb2070b
biooids <biooidsbiooid@gmail.com>
2024-09-22 07:37:46 +0200

    "Updated FlicksPage component to display a warning message with an icon"

--- client/src/components/pages/flicks/FlicksPage.jsx
+++ client/src/components/pages/flicks/FlicksPage.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import "./flicks.css";
 import { Link, Outlet } from "react-router-dom";
 import { Button } from "flowbite-react";
+import { IoIosWarning } from "react-icons/io";
 
 function FlicksPage() {
   return (
     <section className="flex flex-col gap-3   main-flicks-container">
-      <h3>Post and enjoy short flicks content</h3>
+      <div className="flex gap-3 justify-center items-center">
+        <IoIosWarning className="text-lg text-red-700" />
+        <h3 className=" text-red-600">
+          Make sure you pose previous video before opening the other one
+        </h3>
+      </div>
       <nav className="P-3 flex justify-around w-full p-3 bg-slate-700">
         <Link to="." className="p-1 bg-slate-800 rounded-lg">
           All flicks
